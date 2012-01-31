00d7f1088c55f1204efa68c89e42467008bb6731
vladfatu <colegu123@gmail.com>
2012-01-31 10:00:18 +0200

    - added a database helper

---
+++ Work Tracker/src/com/android/tracker/database/DatabaseHelper.java
@@ -0,0 +1,38 @@
+package com.android.tracker.database;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper 
+{
+	
+	ArrayList<String> tables;
+	
+    DatabaseHelper(Context context, String databaseName, int databaseVersion, ArrayList<String> tables) 
+    {
+        super(context, databaseName, null, databaseVersion);
+        this.tables = tables;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) 
+    {
+    	for (String table : tables)
+        db.execSQL(table);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, 
+    int newVersion) 
+    {
+        Log.w("Database", "Upgrading database from version " + oldVersion 
+                + " to "
+                + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS titles");
+        onCreate(db);
+    }
+}
